f61c179e3eeda3d0a425f5224d8d0cb1275a6e4e
karlMoreno <karlcmoreno0811@gmail.com>
2023-11-06 12:08:43 -0800

    basic plan

---
+++ doubLinkedList.cpp
@@ -0,0 +1,17 @@
+// includes here
+// probs just io stream
+
+
+//define node struct
+
+
+//head node
+// tail node
+
+//define list struct
+
+// will contain our functions
+    //traverse
+    //search
+    //delete a node
+    
